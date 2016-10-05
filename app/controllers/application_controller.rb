730a3a59caa1d80e41356d769592916139e40e77
nickangtc <nickangtc@gmail.com>
2016-10-05 17:03:49 +0800

    adds authentication and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def is_authenticated
+    unless current_user
+      flash[:danger] = "Credentials Invalid!"
+      redirect_to login_path
+    end
+  end
+
+  # def is_authenticated_admin
+  #   unless current_user.is_admin
+  #     flash[:danger] = "Credentials Invalid!"
+  #     redirect_to login_path
+  #   end
+  # end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # declare root_path
+
   resources :patients
   resources :doctors
   resources :appointments
