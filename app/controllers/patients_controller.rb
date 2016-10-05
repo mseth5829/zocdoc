bf9bce8714522f268c7770e84d047efdbe0337e7
nickangtc <nickangtc@gmail.com>
2016-10-05 19:46:22 +0800

    adds patient signup

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,13 +1,30 @@
 class PatientsController < ApplicationController
   def show
-  end
-
-  def create
+    @patient = Patient.find(params[:id])
+    @appointments = @patient.appointments
   end
 
   def delete
   end
 
   def new
+    @patient = Patient.new
   end
+
+  def create
+    # puts patient_params.inspect
+
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def patient_params
+      params.require(:patient).permit(:name, :email, :password)
+    end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,17 @@
 class Patient < ApplicationRecord
+  has_secure_password
+
   has_many :appointments
   has_many :doctors, through: :appointments
+
+  validates :email,
+            presence: true,
+            uniqueness: {case_sensitive: false}
+
+  validates :password,
+            length: { in: 8..72 }, on: :create
+
+  def self.authenticate(params)
+    Patient.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
 end
