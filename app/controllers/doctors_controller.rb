31ef23c16261c0718c50a594111a7d5535c193b7
brightchew <bright@homeloan.com.sg>
2016-10-06 11:20:19 +0800

    add doctor_new, create

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,34 @@
 class DoctorsController < ApplicationController
 
   def index
-  	@doctors  =Doctor.all
+  	@doctors  = Doctor.all
   end
 
   def show
-  	@doctor=Doctor.all
+  	@doctor=Doctor.find(params[:id])
   end
+
+  def new
+  	@doctor  = Doctor.new
+  end
+
+  def create
+  	@doctor=Doctor.new(doctor_params)
+
+  	if @doctor.save
+  		redirect_to doctors_path, notice: 'doctor created'
+  	else
+  		render :new
+  	end
+
+  end
+
+  private
+  def doctor_params
+  	params.require(:doctor).permit(:name)
+  end
+
+
 end
+
+  
